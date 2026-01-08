33df670fa5f673e3d5155a484dc0d80bd04465c4
Sanket-bh <sanketbhanuse111@gmail.com>
2026-01-08 15:49:33 +0530

    Initial commit for version1

---
+++ src/main/java/com/foodapp/MainApp.java
@@ -0,0 +1,18 @@
+package com.foodapp;
+
+import com.foodapp.config.AppConfig;
+import com.foodapp.controller.OrderController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        OrderController controller = ctx.getBean(OrderController.class);
+        controller.placeOrder();
+    }
+}
