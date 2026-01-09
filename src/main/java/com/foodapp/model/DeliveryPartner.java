ded4403688c267359ad95a6e1ec0a81f13be8133
Sanket-bh <sanketbhanuse111@gmail.com>
2026-01-09 11:56:20 +0530

    Fix DB creation and table initialization

--- src/main/java/com/foodapp/model/DeliveryPartner.java
+++
@@ -1,4 +0,0 @@
-package com.foodapp.model;
-
-public class DeliveryPartner {
-}
--- src/main/java/com/foodapp/model/Restaurant.java
+++ src/main/java/com/foodapp/model/Restaurant.java
@@ -1,4 +1,9 @@
 package com.foodapp.model;
 
 public class Restaurant {
+    int resId;
+    String location;
+    String name;
+    String address;
+    double ratings;
 }
